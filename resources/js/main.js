ee5806e84564afa8389c3a7b138d0b403c5b928d
Pasquale Parrella <parrellapsquale97@gmail.com>
2024-06-20 16:06:20 +0200

    Scafolding MCR Article

---
+++ resources/js/main.js
@@ -0,0 +1,4 @@
+// Initialization for ES Users
+import { Dropdown, Collapse, initMDB } from "./main.js";
+
+initMDB({ Dropdown, Collapse });
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite ( ['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{$title ?? 'presto.it'}}</title>
+    <!-- Font Awesome -->
+<link
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.css"
+rel="stylesheet"
+/>
+</head>
+<body>
+    
+    <x-nav/>
+    
+
+    <div class="min-vh-100">
+        {{$slot}}
+    </div>
+    
+    
+    <x-foot/>
+
+
+
+    <!-- MDB -->
+<script
+type="text/javascript"
+src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.umd.min.js"
+></script>
+    
+</body>
+</html>
