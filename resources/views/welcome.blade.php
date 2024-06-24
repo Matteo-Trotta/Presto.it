b107a8709d0f3b7777f0c9de39c50f3221a9624c
veronicaronda <vero.ronda@protonmail.com>
2024-06-24 12:00:29 +0200

    sistemato bottone aggiungi annuncio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,9 @@
 
 
     <x-header title='Home page' />
-        <div class="my-3">
+        <div class="my-3 text-center">
             @auth
-                <a class="btn btn-dark" href="{{route('article.create')}}">Aggiungi annuncio</a>
+                <a class="btn btn-outline" href="{{route('article.create')}}"><span class="me-2 fs-6"><i class="fa-solid fa-plus"></i></span>Aggiungi annuncio</a>
             @endauth
         </div>
     <div class="container-fluid">
